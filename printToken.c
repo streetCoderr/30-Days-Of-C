a54576bd339bfba7ac210367dab086eb58303c54
Street Coder <98380817+streetCoderr@users.noreply.github.com>
2022-06-26 09:42:23 +0100

    solve printing token problem

---
+++ printToken.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s));
+
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] == ' ') printf("\n");
+        else printf("%c", s[i]);
+    }
+    free(s);
+    return 0;
+}
